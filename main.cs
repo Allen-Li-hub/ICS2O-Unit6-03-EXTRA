838ee92346040955328aca519bf915e21ed0804a
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2023-01-23 15:34:19 -0500

    change

--- main.cs
+++ main.cs
@@ -1,40 +1,43 @@
+
+
 // Created by: Allen Li
-// Created on: Oct 2022
+// Created on: Dec 2022
 //
-// This program displays, "Multiplication Calculator"
+//This program displays temperature checker
 
 using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.IO;
+
 class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main()
     {
-        System.Random random = new System.Random();
-        double firstNumber;
-        double secondNumber;
-        Console.Write("Multiplication Calculator");
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
         Console.WriteLine("");
-        Console.Write("Input two number to find out the product");
+        JsonNode forecastNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(forecastNode);
+        JsonNode mainNode = forecastNode!["main"]!;
+        // Console.WriteLine(windNode);
+        JsonNode tempNode = mainNode!["temp"]!;
+        string temp = tempNode.ToString();
+        double finalTemp = Convert.ToDouble(temp) - 273.6;
+        Console.WriteLine("Current outside temperature: " + finalTemp.ToString("0.0") + "°C");
+        // Declares for file
+        string text = "Total: " + finalTemp.ToString("0.0");
+        // Makes file, added code
+        await File.WriteAllTextAsync("WriteText.txt", text);
+        Console.WriteLine("File Updated...");
         Console.WriteLine("");
-        Console.Write("Input the first number:");
-        firstNumber = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Input the second number: ");
-        secondNumber = Convert.ToDouble(Console.ReadLine());
-        double product = 0;
-        double counter = 0;
-        while (counter < Math.Abs(secondNumber))
-        {
-            counter++;
-            product = product + Math.Abs(firstNumber);
-        }
-        if ((firstNumber < 0 && secondNumber > 0) || (secondNumber < 0 && firstNumber > 0))
-        {
-            Console.WriteLine("-" + product);
-        }
-        else
-        {
-            Console.WriteLine(product);
-        }
-        Console.WriteLine("");
-        Console.Write("/Done");
+
+        Console.WriteLine("\nDone.");
     }
 }
